d2601b2bbb7813b2e61528f366d12535131bdb0e
Kike <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-11-02 19:39:39 +0100

    Creado archivo del inventario de coches actuales

---
+++ scripts/carModels.js
@@ -0,0 +1,16 @@
+export { inventory }
+
+// Inventario inicial
+const inventory = [
+  { id: 1, marca: "Cupra", modelo: "Formentor", cantidad: 12, precio: 37.0 },
+  { id: 2, marca: "Cupra", modelo: "Leon", cantidad: 10, precio: 32.3 },
+  { id: 3, marca: "Cupra", modelo: "Born", cantidad: 3, precio: 42.7 },
+  { id: 4, marca: "Cupra", modelo: "Tavascan", cantidad: 3, precio: 53.6 },
+  { id: 5, marca: "Seat", modelo: "Ibiza", cantidad: 5, precio: 17.0 },
+  { id: 6, marca: "Seat", modelo: "Leon", cantidad: 10, precio: 25.5 },
+  { id: 7, marca: "Seat", modelo: "Arona", cantidad: 12, precio: 19.7 },
+  { id: 8, marca: "Seat", modelo: "Ateca", cantidad: 7, precio: 31.32 },
+  { id: 9, marca: "Seat", modelo: "Tarraco", cantidad: 4, precio: 33.4 },
+  { id: 10, marca: "Seat", modelo: "Alhambra", cantidad: 1, precio: 34.0 },
+  { id: 11, marca: "Seat", modelo: "Leon", cantidad: 10, precio: 25.5 },
+]
